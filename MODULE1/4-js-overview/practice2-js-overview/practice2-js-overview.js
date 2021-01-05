9ff26bcc7db013806e08df7b1358550b47ad7d7c
Khadeve <hoquangkha95@gmail.com>
2021-01-05 11:51:12 +0700

    Update JS practice

---
+++ MODULE1/4-js-overview/practice2-js-overview/practice2-js-overview.js
@@ -0,0 +1,12 @@
+function showMessage() {
+    document.getElementById("message").innerHTML="<b>This is a message!<b>"
+}
+function showAlert() {
+    alert("Alert Here!");
+}
+function writeMessage() {
+    document.write("Message is written here!");
+}
+function showOnConsole() {
+    console.log("Message On Console!");
+}
