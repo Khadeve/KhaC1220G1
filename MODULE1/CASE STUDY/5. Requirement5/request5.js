f8d2ce3418e896c63c72ffb746535433e4cc2174
Khadeve <hoquangkha95@gmail.com>
2021-01-19 22:02:17 +0700

    Update OOP

--- MODULE1/CASE STUDY/5. Requirement5/request5.js
+++ MODULE1/CASE STUDY/5. Requirement5/request5.js
@@ -1,7 +1,3 @@
-let mainMenu =
-  "1. Add New Customer\n2. Display Customer Information\n3. Edit Customer Information\n4. Delete Customer\n5. Exit\n\n";
-
-let customerListArr = [];
 let info = [
   "fullname",
   "id",
@@ -30,31 +26,43 @@ let title = [
 ];
 let amountOfInfo = info.length;
 
-function Customer(
-  fullname,
-  id,
-  dayOfBirth,
-  email,
-  address,
-  membership,
-  discount,
-  children,
-  rentDays,
-  serviceType,
-  roomType
-) {
-  this.fullname = fullname;
-  this.id = id;
-  this.dayOfBirth = dayOfBirth;
-  this.email = email;
-  this.address = address;
-  this.membership = membership;
-  this.discount = discount;
-  this.children = children;
-  this.rentDays = rentDays;
-  this.serviceType = serviceType;
-  this.roomType = roomType;
+class Customer {
+  constructor(
+    fullname,
+    id,
+    dayOfBirth,
+    email,
+    address,
+    membership,
+    discount,
+    children,
+    rentDays,
+    serviceType,
+    roomType
+  ) {
+    this.fullname = fullname;
+    this.id = id;
+    this.dayOfBirth = dayOfBirth;
+    this.email = email;
+    this.address = address;
+    this.membership = membership;
+    this.discount = discount;
+    this.children = children;
+    this.rentDays = rentDays;
+    this.serviceType = serviceType;
+    this.roomType = roomType;
+  }
+  get cusInfo() {
+    let information = "";
+    for (let i = 0; i < amountOfInfo; i++) {
+      information += i + 1 + ". " + title[i] + ": " + this[info[i]] + "<br>";
+    }
+    return information;
+  }
 }
+
+// let customer1 = new Customer("3", 1, "1", "1", "1", "1", 1, 1, 1, "1", "2");
+let customerListArr = [];
 
 function openPage(pageName, elmnt, color) {
   let tabContent, tabLink;
@@ -76,132 +84,49 @@ function getRadioValue(radioValue) {
   document.getElementById("radioValue").value = radioValue;
 }
 
-function getCustomerInfo() {
-  let customer = new Customer("", 0, "", "", "", "", 0, 0, 0, "", "");
+function getCustomerObject() {
+  let customer = new Customer();
   for (let i = 0; i < amountOfInfo; i++) {
     if (i == 5) {
       customer.membership = document.getElementById("radioValue").value;
-      alert(customer.membership);
     } else {
       customer[info[i]] = document.getElementById(info[i]).value;
     }
   }
-  console.log(customer);
   return customer;
 }
 
-function getCustomerList(list) {
+function addNewCustomerToList() {
+  let newCustomer = getCustomerObject();
+  if (confirm("Confirm adding new customer to the list.")) {
+    customerListArr.push(newCustomer);
+    alert("Completed");
+    resetInputInForm("customerForm");
+  }
+}
+
+function getCustomerList() {
   let content = "";
-  let amountOfCustomer = list.length;
+  let amountOfCustomer = customerListArr.length;
   for (let i = 0; i < amountOfCustomer; i++) {
-    content += "Customer" + (i + 1) + ":\n" + getInfoOfCustomer(list[i]) + "\n";
+    content += "<b>Customer" + (i + 1) + ":</b><br>" + customerListArr[i].cusInfo + "<br>";
   }
   return content;
 }
 
-function addNewCustomerToList(list, newCustomer) {
-  list.push(newCustomer);
+function resetInputInForm(formId) {
+  document.getElementById(formId).reset();
 }
 
-function editCustomer(customer) {
-  let c;
-  do {
-    c = false;
-    let flag, editOpt;
-    do {
-      flag = false;
-      editOpt = prompt(
-        getInfoOfCustomer(customer) +
-          "\n***Please enter the index of info you want to edit:"
-      );
-      if (editOpt != null) {
-        if (editOpt < 1 || editOpt > amountOfInfo || isNaN(editOpt)) {
-          alert("Invalid index");
-          flag = true;
-        }
-      } else {
-        break;
-      }
-    } while (flag);
-
-    //Checking if Cancel is entered.
-    if (editOpt == null) {
-      if (confirm("Do you want to exit editing operation?")) {
-        break;
-      } else {
-        c = true;
-        continue;
-      }
-    }
-
-    for (let i = 0; i < amountOfInfo; i++) {
-      if (editOpt == i + 1) {
-        if (i == 5) {
-          customer[info[i]] = prompt(
-            "***Enter a new info:\n1. Member\n2. Silver\n3. Gold\n4. Platinum\n5. Diamond\n"
-          );
-        } else if (i == 9) {
-          customer[info[i]] = prompt(
-            "***Enter a new info:\n1. Room\n2. House\n3. Villa\n"
-          );
-        } else if (i == 10) {
-          customer[info[i]] = prompt(
-            "***Enter a new info:\n1. Normal\n2. Business\n3. Vip\n"
-          );
-        } else {
-          customer[info[i]] = prompt("***Enter a new info:");
-        }
-        break;
-      }
-    }
-    c = confirm("Update successfully. Do you want to continue to edit?");
-  } while (c);
+function displayCustomerList() {
+  let content = getCustomerList();
+  if (content != "") {
+    document.getElementById("content").innerHTML = getCustomerList();
+  }
 }
 
-function editCustomerInList(list) {
-  let c;
-  do {
-    // let choice = "";
-    // let listLen = list.length;
-    // //Creating choices based on customer names.
-    // for (let i = 0; i < listLen; i++) {
-    //   choice += i + 1 + ". " + list[i].fullname + "\n";
-    // }
-    c = false;
-    let listLen = list.length;
-    //Checking whether the index entered is valid.
-    let flag, editIndex;
-    do {
-      flag = false;
-      editIndex = prompt(
-        getCustomerList(list) + "***Please enter the index of customer:"
-      );
-      if (editIndex != null) {
-        if (editIndex < 1 || editIndex > listLen || isNaN(editIndex)) {
-          alert("Invalid index");
-          flag = true;
-        }
-      } else {
-        break;
-      }
-    } while (flag);
 
-    //Checking if Cancel is entered.
-    if (editIndex == null) {
-      if (confirm("Do you want to exit editing operation?")) {
-        break;
-      } else {
-        c = true;
-        continue;
-      }
-    }
-    //Editing customer with the corresponding index.
-    editCustomer(list[parseInt(editIndex) - 1]);
-    c = confirm(
-      "Editing the customer successfully.\nDo you want to continue to edit another customer?"
-    );
-  } while (c);
-}
-
+document.getElementById("defaultOpen").click();
 //Get the element with id="defaultopen" and click on it.
-document.getElementById("defaultopen").click();
+//document.getElementById("defaultopen").click();
+//getCustomerList();
